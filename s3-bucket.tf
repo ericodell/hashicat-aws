b71879ce3762bf65e814e37e4830106bba1b32f2
Eric Odell <cirelledo@gmail.com>
2022-01-18 13:27:33 -0800

    s3 module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket-1" {
   bucket_prefix = var.prefix
-  source  = "app.terraform.io/sdsc-sherlock-test/s3-bucket-1/aws"
-  version = "2.2.0"
+  source        = "app.terraform.io/sdsc-sherlock-test/s3-bucket/aws"
+  version       = "2.2.0"
   # insert required variables here
 }
